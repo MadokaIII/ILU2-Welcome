a58b4183f014b5ad5f84853eb4c4fd7a693c61b3
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2023-04-28 15:01:33 +0200

    Exigence 2 fini

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -5,20 +5,28 @@ public class Welcome {
     private static final String HELLO = "Hello, ";
     private static final String FRIEND = "my friend";
 
-    private static String capitalize(String input){
-        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    private static String capitalize(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 
-    private static String friend(String input){
-        if (input == null || "".equals(input.trim())){
+    private static String friend(String input) {
+        if (input == null || "".equals(input.trim())) {
             return FRIEND;
-        }
-        else return capitalize(input.trim());
+        } else return capitalize(input.trim());
     }
 
-    public static String welcome(String input){
-        if (input != null && !"".equals(input.trim()) && input.toUpperCase().equals(input)) return HELLO.toUpperCase() + friend(input).toUpperCase() + " !";
-        return HELLO + friend(input);
+    private static boolean yell(String input) {
+        return input.toUpperCase().equals(input);
+    }
+
+    public static String welcome(String input) {
+        StringBuilder str = new StringBuilder(HELLO);
+        if (yell(friend(input))){
+            str.append(friend(input) + " !");
+            return str.toString().toUpperCase();
+        }
+        str.append(friend(input));
+        return str.toString();
     }
 
 }
