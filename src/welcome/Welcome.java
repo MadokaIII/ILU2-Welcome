f6507c9b8986a8bdeb62dab7f2e6c82316b64fe7
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2023-04-28 14:30:23 +0200

    Exigence 1.3

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -2,8 +2,14 @@ package welcome;
 
 public class Welcome {
 
+    private static final String HELLO = "Hello, ";
+
+    private static String capitalize(String input){
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+
     public static String welcome(String input){
-        return "Hello, Joe";
+        return HELLO + capitalize(input);
     }
 
 }
